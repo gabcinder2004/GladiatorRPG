3eec603af5ae075822bd016566f5702ed0e810c8
Gabriel Nunez <Gabriel_Nunez@ultimatesoftware.com>
2015-11-26 15:33:40 -0500

    Initial commit

---
+++ RandomRPG/RandomRPG/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RPG Game v0.01");
+        }
+    }
+}
